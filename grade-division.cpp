d6a33562368e8ee79e845653baa45b76a517f123
SHOVAN BERA <157379408+theshovan@users.noreply.github.com>
2025-04-10 23:41:17 +0530

    Update grade-division.cpp

--- grade-division.cpp
+++ grade-division.cpp
@@ -1,6 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+/*Marks>=80, Print grade A,
+Marks>=60, Print grade B,
+Marks>=40, Print grade C,
+Marks>=25, Print grade D,
+Marks<25, Print grade F
+    */
+
 int main(){
     int marks;
     cout<<"Enter your marks:";
